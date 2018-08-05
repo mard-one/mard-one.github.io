8656ac3dc3b720a6965ba9a4c337ef2e02652492
Mardon Mashrabov <mardon.mashrabov@gmail.com>
2018-08-05 12:11:37 +0400

    deploy12

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var transporter = nodemailer.createTransport({
 // app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(__dirname + 'client/dist'));
 var router = express.Router();
 app.post('/getintouch', function (req, res) {
     var mailOptions = {
@@ -52,7 +52,7 @@ app.post('/getintouch', function (req, res) {
     });
 });
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(__dirname + 'client/dist/index.html');
 });
 app.set('port', process.env.PORT || 8080);
 app.listen(app.get('port'), function () {
